717d645af9950ee1b4611bf265b7a01f168f4d47
Hussain Nawaz Lalee <38792700+Lalee10@users.noreply.github.com>
2019-04-28 20:13:46 +0500

    commit

---
+++ src/expressivo/ParserError.java
@@ -0,0 +1,16 @@
+package expressivo;
+
+public class ParserError extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4315981621734481144L;
+
+	/**
+	 * This class throws error for Parser.java
+	 */
+
+	public ParserError(String message) {
+		super(message);
+	}
+}
